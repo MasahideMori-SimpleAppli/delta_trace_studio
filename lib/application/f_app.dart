8cbbfb07e2b0a76eeeebf049f8cd8ebc6c59df72
MasahideMori-SimpleAppli <59500584+MasahideMori-SimpleAppli@users.noreply.github.com>
2025-11-24 20:34:38 +0900

    Update f_app.dart

--- lib/application/f_app.dart
+++ lib/application/f_app.dart
@@ -1,7 +1,7 @@
 /// アプリケーション全体で使用するフィールド定義
 class FApp {
   static const String appName = "DeltaTraceStudio";
-  static const String appVersion = "0.0.6";
+  static const String appVersion = "0.0.7";
   static const String appLegalese = "2025 Masahide Mori";
-  static const String lastUpdate = "Last update：2025/10/19 (Sun.)";
+  static const String lastUpdate = "Last update：2025/11/24 (Mon.)";
 }
