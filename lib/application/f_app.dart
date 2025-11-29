b1686998b878aa5c144d03f4655cac4165887fd2
MasahideMori-SimpleAppli <59500584+MasahideMori-SimpleAppli@users.noreply.github.com>
2025-11-29 16:07:59 +0900

    v0.0.40.

--- lib/application/f_app.dart
+++ lib/application/f_app.dart
@@ -3,5 +3,5 @@ class FApp {
   static const String appName = "DeltaTraceStudio";
   static const String appVersion = "0.0.6";
   static const String appLegalese = "2025 Masahide Mori";
-  static const String lastUpdate = "Last update：2025/10/19 (Sun.)";
+  static const String lastUpdate = "Last update：2025/11/29 (Sat.)";
 }
