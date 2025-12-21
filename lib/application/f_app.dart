cf6c70641758241830dc5fbc7d5eebad5d9bacd3
MasahideMori-SimpleAppli <59500584+MasahideMori-SimpleAppli@users.noreply.github.com>
2025-12-21 20:48:30 +0900

    v0.2.1, for dtdb v0.1.1.

--- lib/application/f_app.dart
+++ lib/application/f_app.dart
@@ -1,7 +1,7 @@
 /// アプリケーション全体で使用するフィールド定義
 class FApp {
   static const String appName = "DeltaTraceStudio";
-  static const String appVersion = "0.2.0";
+  static const String appVersion = "0.2.1";
   static const String appLegalese = "2025 Masahide Mori";
   static const String lastUpdate = "Last update：2025/12/20 (Sat.)";
 }
